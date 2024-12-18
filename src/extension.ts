870e4f7d5a10053bda64b127591a0ba8171086eb
Austin Nelson <nelsonhaustin@gmail.com>
2024-12-17 22:23:59 -0800

    Got extension to run when a different branch is checked out

--- src/extension.ts
+++ src/extension.ts
@@ -1,51 +1,46 @@
 import * as vscode from 'vscode';
-import simpleGit, { SimpleGit } from 'simple-git';
 
 /**
  * This function gets called when the extension is activated.
- * It registers the command as 'git-tab-tracker.getCurrentBranch'.
  * 
  * @param context - Context provided by VS Code.
  */
-export async function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('git-tab-tracker.getCurrentBranch', async () => {
-		await trackTabs();
-	});
+export function activate(context: vscode.ExtensionContext) {
+    const gitExtension = vscode.extensions.getExtension('vscode.git')?.exports;
+    const api = gitExtension?.getAPI(1);
 
-	context.subscriptions.push(disposable);
+    if (!api) {
+        vscode.window.showErrorMessage("Git API not found!");
+        return;
+    }
+
+    const handleBranchChange = (repo: any) => {
+        const head = repo.state.HEAD;
+
+        if (head) {
+            const { name } = head;
+            vscode.window.showInformationMessage(`Switched to branch: ${name}`);
+        } else {
+            vscode.window.showInformationMessage("No branch is currently checked out.");
+        }
+    };
+
+    const attachListeners = (repo: any) => {
+        context.subscriptions.push(
+            repo.state.onDidChange(() => handleBranchChange(repo))
+        );
+    };
+
+    api.repositories.forEach((repo: any) => attachListeners(repo));
+
+    context.subscriptions.push(
+        api.onDidOpenRepository((repo: any) => attachListeners(repo))
+    );
+
+    api.repositories.forEach(handleBranchChange);
 }
 
 /**
  * This function is called when the extension is deactivated.
  */
 export function deactivate() {}
-
-async function trackTabs() {
-	let workspacePath = await getWorkspaceFolder();
-	if (!workspacePath) {
-		return;
-	}
-
-	getGitBranch(workspacePath);
-}
-
-async function getWorkspaceFolder(): Promise<string | undefined>{
-	const workspaceFolders = vscode.workspace.workspaceFolders;
-	if(!workspaceFolders){
-		vscode.window.showErrorMessage('No workspace open!');
-		return;
-	}
-
-	return workspaceFolders[0].uri.fsPath;
-}
-
-async function getGitBranch(folderPath: string) {
-	const git: SimpleGit = simpleGit(folderPath);
-	try {
-		const branchSummary = await git.branch();
-		const currentBranch = branchSummary.current;
-		vscode.window.showInformationMessage(`Current branch: ${currentBranch}`);
-	} catch (error) {
-		vscode.window.showErrorMessage('Failed to get current branch: ');
-	}
-}
